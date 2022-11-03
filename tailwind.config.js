e4f6876bd271e797d546be18c676174b8709570b
Zeglius <33781398+Zeglius@users.noreply.github.com>
2022-11-03 17:14:08 +0100

    Removed tailwindcss preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {},
   },
